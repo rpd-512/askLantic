57300661cff7661a75498696834d06e7888c6e39
Rhiddhi Prasad Das <rhiddhiprasad@gmail.com>
2021-04-24 22:38:26 +0530

    Update pdoData.php

--- pdoData.php
+++ pdoData.php
@@ -1,7 +1,7 @@
 <?php
 $dbhs = "localhost";
-$dbus = "askLanticAdmin";
+$dbus = ""; //database username
 $dbnm = "askLantic";
-$dbpw = "ASKlantic@39039820";
+$dbpw = ""; // database password
 $pdo=new PDO('mysql:host='.$dbhs.';port=3306;dbname='.$dbnm.';charset=utf8mb4',$dbus,$dbpw);
 ?>
